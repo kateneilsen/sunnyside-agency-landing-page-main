9f0e0a303e3ba0a103ecbbeaee631694e162e349
Kate <neilsenk09@gmail.com>
2023-10-24 11:17:42 -0400

    footer complete

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,28 @@
-import React from "react";
-import Logo from "../images/logo.svg";
+//import Logo from "../images/logo.svg";
 import "./Footer.css";
+import Facebook from "../images/icon-facebook.svg";
+import Instagram from "../images/icon-instagram.svg";
+import Pinterest from "../images/icon-pinterest.svg";
+import Twitter from "../images/icon-twitter.svg";
+import Logo from "./Logo.jsx";
 
 const Footer = () => {
   return (
     <footer>
-      <img src={Logo} alt="Logo" className="logo" />
+        <Logo fill={"#2C7566"} className={"logo"}/>
+        <div className={"nav-links"}>
+            <a href={"/"}>About</a>
+            <a href={"/"}>Services</a>
+            <a href={"/"}>Projects</a>
+        </div>
+        <div className={"socials"}>
+            <a href={"/"}>
+                <img src={Facebook} alt={"facebook"} />
+            </a>
+            <a href={"/"}><img src={Instagram} alt={"instagram"} /></a>
+            <a href={"/"}><img src={Twitter} alt={"twitter"} /></a>
+            <a href={"/"}><img src={Pinterest} alt={"pinterest"} /></a>
+        </div>
     </footer>
   );
 };
