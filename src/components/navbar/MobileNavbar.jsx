561e950b9c17b403623ecd9bfe18867882d9cd5d
Kate <neilsenk09@gmail.com>
2023-10-25 11:20:55 -0400

    headlessui for mobile nav menu

--- src/components/navbar/MobileNavbar.jsx
+++ src/components/navbar/MobileNavbar.jsx
@@ -1,21 +1,40 @@
 
-import Menu from "../../images/icon-hamburger.svg";
+import MenuIcon from "../../images/icon-hamburger.svg";
 import "./MobileNavbar.css";
-import NavLinks from "./NavLinks.jsx";
+import {Link} from "react-router-dom";
 import Logo from "../Logo.jsx";
+import { Menu } from '@headlessui/react'
 
-// eslint-disable-next-line react/prop-types
+// eslint-disable-next-line react/prop-types,no-unused-vars
 const MobileNavbar = ({visible, setVisible, color, windowSize}) => {
   return (
         <nav style={{background: color}}>
           <a href={"/"}>
             <Logo fill={"white"} width={windowSize >= 768 ? "170" : "124"} height={windowSize >= 768 ? "33" : "24"} />
           </a>
-          <button onClick={() => setVisible(!visible)}>
-            <img src={Menu} alt="menu" />
-          </button>
+            <Menu>
+                <Menu.Button className={"menu-button"}>
+                    <img src={MenuIcon} alt="menu" />
+                </Menu.Button>
+                <Menu.Items className={"menu-items"}>
+                    <Menu.Item className={"menu-item"}>
+                            <Link to="/about">
+                               About
+                            </Link>
+                    </Menu.Item>
+                    <Menu.Item className={"menu-item"}>
+                        <Link to={"/services"}>Services</Link>
+                    </Menu.Item>
+                    <Menu.Item className={"menu-item"}>
+                        <Link to={"/projects"}>Projects</Link>
+                    </Menu.Item>
+                    <Menu.Item>
+                        <button className={"contact"}>Contact</button>
+                    </Menu.Item>
+                </Menu.Items>
+            </Menu>
 
-          {!visible ? "" : <NavLinks visible={visible} />}
+          {/*{!visible ? "" : <NavLinks visible={visible} />}*/}
 
         </nav>
   );
--- src/components/navbar/NavLinks.jsx
+++ src/components/navbar/NavLinks.jsx
@@ -1,26 +1,54 @@
 import "./NavLinks.css";
 import {Link} from "react-router-dom";
+import { Menu } from '@headlessui/react'
 
 // eslint-disable-next-line react/prop-types,no-unused-vars
 function NavLinks({visible}) {
     return (
-        <div className={`nav-links ${visible ? "show" : ""}`}>
-            <ul>
-                <li>
-                    <Link to={"/about"}>About</Link>
-                </li>
-                <li>
-                    <Link to={"/services"}>Services</Link>
-                </li>
-                <li>
-                    <Link to={"/projects"}>Projects</Link>
-                </li>
-                <li>
-                    <button className={"contact"}>Contact</button>
-                </li>
-            </ul>
-        </div>
-
+        // <div className={`nav-links ${visible ? "show" : ""}`}>
+        //     <ul>
+        //         <li>
+        //             <Link to={"/about"}>About</Link>
+        //         </li>
+        //         <li>
+        //             <Link to={"/services"}>Services</Link>
+        //         </li>
+        //         <li>
+        //             <Link to={"/projects"}>Projects</Link>
+        //         </li>
+        //         <li>
+        //             <button className={"contact"}>Contact</button>
+        //         </li>
+        //     </ul>
+        // </div>
+        <Menu>
+            <Menu.Button>More</Menu.Button>
+            <Menu.Items>
+                <Menu.Item>
+                    {({ active }) => (
+                        <a
+                            className={`${active && 'bg-blue-500'}`}
+                            href="/account-settings"
+                        >
+                            Account settings
+                        </a>
+                    )}
+                </Menu.Item>
+                <Menu.Item>
+                    {({ active }) => (
+                        <a
+                            className={`${active && 'bg-blue-500'}`}
+                            href="/account-settings"
+                        >
+                            Documentation
+                        </a>
+                    )}
+                </Menu.Item>
+                <Menu.Item disabled>
+                    <span className="opacity-75">Invite a friend (coming soon!)</span>
+                </Menu.Item>
+            </Menu.Items>
+        </Menu>
     );
 }
 
